9b02336ebd6d0bd741aef8932adcae4ee0776c5f
Putu Prema <iputupremaananda@gmail.com>
2025-11-21 21:26:26 +0700

    Base model for pages

---
+++ internal/viewmodel/metadata.go
@@ -0,0 +1,13 @@
+// Package viewmodel
+package viewmodel
+
+type Metadata struct {
+	Title          string
+	Description    string
+	Keywords       []string
+	Authors        []string
+	OpenGraphTitle string
+	OpenGraphDesc  string
+	TwitterTitle   string
+	TwitterDesc    string
+}

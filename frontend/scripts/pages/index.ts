e4580d4fbeb673efca2916070bf2265fc86b1f8c
Putu Prema <iputupremaananda@gmail.com>
2025-11-30 05:59:40 +0700

    Update recipe card JS initialization logic

--- frontend/scripts/pages/index.ts
+++ frontend/scripts/pages/index.ts
@@ -1,5 +1,11 @@
 import * as recipeCard from "../shared-components/recipe/recipe-card";
 
 document.addEventListener("DOMContentLoaded", () => {
-	recipeCard.initialize();
+	// Bind to all recipe cards in the Trending Now section
+	const recipeCards = document.querySelectorAll<HTMLDivElement>(
+		'[data-component-type="RecipeCard"]',
+	);
+	recipeCards.forEach((card) => {
+		recipeCard.initialize(card);
+	});
 });
